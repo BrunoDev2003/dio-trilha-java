0640eb2ac8b7554587289e2d42335b127077b476
Bruno Gusmão <brunogusmao43@gmail.com>
2022-07-18 15:48:01 -0300

    tipos de variaveis que podem ser alterados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,8 +1,17 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        double VALOR_DE_PI = 3.1415;
+
+        VALOR_DE_PI = 10.75;
     }
 }
