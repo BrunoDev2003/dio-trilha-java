8a325ef9e4608a79e11b013902e77863995eb0df
Bruno Gusmão <brunogusmao43@gmail.com>
2022-07-25 14:27:50 -0300

    incrementando e invertendo valores de variaveis

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,15 @@
+public class Operadores2 {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+
+        System.out.println(numero ++);
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
