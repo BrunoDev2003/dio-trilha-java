bd3a039679f7f027c56f0459571a47518430c340
Bruno Gusmão <brunogusmao43@gmail.com>
2023-07-28 21:37:00 -0300

    gerando os Getters e Setters e o ToString.

--- src/dominio/Curso.java
+++ src/dominio/Curso.java
@@ -1,7 +1,31 @@
 package dominio;
 
 public class Curso {
-	String titulo;
-	String descricao;
-	String carga_horaria;
+	private String titulo;
+	private String descricao;
+	private String carga_horaria;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCarga_horaria() {
+		return carga_horaria;
+	}
+	public void setCarga_horaria(String carga_horaria) {
+		this.carga_horaria = carga_horaria;
+	}
+	@Override
+	public String toString() {
+		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", carga_horaria=" + carga_horaria + "]";
+	}
+	
+	
 }
--- src/dominio/Mentoria.java
+++ src/dominio/Mentoria.java
@@ -1,9 +1,35 @@
 package dominio;
 
 public class Mentoria {
-	String titulo;
-	String descricao;
-	int cargaHoraria;
+	private String titulo;
+	private String descricao;
+	private int cargaHoraria;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	@Override
+	public String toString() {
+		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+	
+	
+	
+	
 }
 
-//04:10
+//6:19
