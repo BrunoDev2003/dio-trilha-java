62f5405717b009a0be3bd62d182c9af97508df55
Bruno Gusmão <brunogusmao43@gmail.com>
2023-08-09 20:58:05 -0300

    implementando a classe bootcamp.

--- src/dominio/bootcamp.java
+++ src/dominio/bootcamp.java
@@ -1,5 +1,15 @@
 package dominio;
 
-public class bootcamp {
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+public class bootcamp {
+	private String nome;
+	private String descricao;
+	private LocalDate dataInicial = LocalDate.now();
+	private LocalDate dataFinal = dataInicial.plusDays(45);
+	private Set<dev> devsInscritos = new HashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
 }
